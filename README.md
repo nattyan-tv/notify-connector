cf970afbf1b7590f73133f39fc303c93f20e8283
nattyan-tv <73095116+nattyan-tv@users.noreply.github.com>
2022-10-01 18:27:07 +0900

    Discord用メソッド追加

--- README.md
+++ README.md
@@ -2,9 +2,17 @@
 通知をぶっとばすもの。
 
 # Detail
-`LINE Notify`へのメッセージ送信を一般的なGETリクエストで行うことが出来るいわゆるコネクター。  
+`LINE Notify`とかへのメッセージ送信を一般的なGETリクエストで行うことが出来るいわゆるコネクター。  
 あくまで個人用。（認証を行わないため。）
+
+# Endpoint
+各エンドポイントの解説
+
+エンドポイント|解説|要求内容
+---|---|---
+`/line`|LINE Notifyにメッセージ送信|`message=<送信したいメッセージ内容>`
+`/discord`|Discordにメッセージ送信|`content=<送信したいメッセージ内容>&username=<ユーザー名>&avatar_url=<アイコン画像>`
 
 # Q&A
 - Q.いやいや、こんなん何に使うんだよ
-  A.まぁ...うん...Microsoft PowerAutomateのHTTP要求がPREMIUM専用の機能だから、OneDriveの「URLからのファイルのアップロード」(Upload file from URL to Onedrive)が、実は無料で使えるGET要求が出来るコネクタだから、それを使用することでTeamsのチャネルに送られたメッセージをこの「Notify Connector」にGETで飛ばせば、LINE NotifyにてTeamsのメッセージを送信したりするくらいのことはもしかしたら出来るかもしれませんね知りませんけど。
+  A.まぁ...うん...Microsoft PowerAutomateのHTTP要求がPREMIUM専用の機能だから、OneDriveの「URLからのファイルのアップロード」(Upload file from URL to Onedrive)が、実は無料で使えるGET要求が出来るコネクタだから、それを使用することでTeamsのチャネルに送られたメッセージをこの「Notify Connector」にGETで飛ばせば、LINE NotifyとかDiscordでTeamsのメッセージを見たりするくらいのことはもしかしたら出来るかもしれませんね知りませんけど...
--- main.py
+++ main.py
@@ -6,24 +6,33 @@ import json
 
 app = sanic.Sanic(__name__)
 
-URL = "https://notify-api.line.me/api/notify"
+LINE_NOTIFY_URL = "https://notify-api.line.me/api/notify"
 
 SETTING = json.load(open("secret.json", "r"))
-TOKEN = SETTING["token"]
+TOKEN = SETTING["line_token"]
+DISCORD_WEBHOOK_URL = SETTING["discord_webhook"]
 
 
 @app.route("/")
 async def main_root(request: sanic.Request):
     return sanic.response.json({"Service":"Available"})
 
-@app.route("/notify")
+@app.route("/line")
 async def line_notify(request: sanic.Request):
     if request.args.get("message") is None:
         return sanic.response.json({"status":400, "message":"Message is None."})
     async with aiohttp.ClientSession() as session:
-        async with session.post(URL, headers={"Authorization": f"Bearer {TOKEN}"} , data={"message": request.args.get("message")}) as response:
-            print(response.status)
+        async with session.post(LINE_NOTIFY_URL, headers={"Authorization": f"Bearer {TOKEN}"}, data={"message": request.args.get("message")}) as response:
             return sanic.response.json(await response.json())
+
+@app.route("/discord")
+async def discord_notify(request: sanic.Request):
+    _content = request.args.get("content")
+    if _content is None:
+        return sanic.response.json({"status":400, "message":"Content is None."})
+    async with aiohttp.ClientSession() as session:
+        await session.post(DISCORD_WEBHOOK_URL, data={"content": _content, "username": request.args.get("username", ""), "avatar_url": request.args.get("avatar_url", "")})
+        return sanic.response.json({"status": 200, "content": _content})
 
 if __name__ == "__main__":
     app.run(host=SETTING["host"], port=SETTING["port"])
